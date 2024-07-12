b3b4a6e4ab915d646a4675d33db9c0798d2911ea
Aryan Kapoor <64773763+Aryankpoor@users.noreply.github.com>
2024-07-12 14:45:08 +0530

    Update problem.js

--- problem.js
+++ problem.js
@@ -3,6 +3,6 @@
 // The first digit of the number is 4 , 3rd digit is 1 and 6th digit is 1.
 // Which looks something like 4x1xx1xx
 // We just have to find the rest of the digits
-// If you want to see the visual problem I have put a photo in the root directory named problem.jpg
+// If you want to see the visual problem I have put a photo in the root directory named problem.png
 
 '1'.joi(['4','3','7'].map(x=>str(atoi(2*x)+1)))
